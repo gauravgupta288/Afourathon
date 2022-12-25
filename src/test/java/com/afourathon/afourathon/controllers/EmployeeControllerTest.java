fa4cc2e75cada124c3ead5a13e9a2d352e730dd6
guptagaurav288 <guptagaurav288@gmail.com>
2022-12-25 22:53:34 +0530

    Unit test

---
+++ src/test/java/com/afourathon/afourathon/controllers/EmployeeControllerTest.java
@@ -0,0 +1,24 @@
+package com.afourathon.afourathon.controllers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EmployeeControllerTest {
+
+    @Test
+    void getAllEmployees() {
+    }
+
+    @Test
+    void getAEmployee() {
+    }
+
+    @Test
+    void addEmployee() {
+    }
+
+    @Test
+    void updateEmployee() {
+    }
+}
---
+++ src/test/java/com/afourathon/afourathon/controllers/SkillsControllerTest.java
@@ -0,0 +1,24 @@
+package com.afourathon.afourathon.controllers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SkillsControllerTest {
+
+    @Test
+    void getSkills() {
+    }
+
+    @Test
+    void addSkills() {
+    }
+
+    @Test
+    void updateASkill() {
+    }
+
+    @Test
+    void deleteASkill() {
+    }
+}
