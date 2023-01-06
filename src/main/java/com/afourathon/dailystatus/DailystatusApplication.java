6778d997b8f2fdf1cb22b51ea54094d3f1f864e9
guptagaurav288 <guptagaurav288@gmail.com>
2023-01-06 13:35:53 +0530

    new conf

---
+++ src/main/java/com/afourathon/dailystatus/DailystatusApplication.java
@@ -0,0 +1,12 @@
+package com.afourathon.dailystatus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailystatusApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(DailystatusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/afourathon/dailystatus/DailystatusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.afourathon.dailystatus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailystatusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
